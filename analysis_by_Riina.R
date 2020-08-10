0da9cf79e2dd4b2649f2ba69cfd599dccf661ab9
klaispeets <69356602+klaispeets@users.noreply.github.com>
2020-08-10 10:52:17 +0300

    Update analysis_by_Riina.R
    Võtsin full mudeli ära, polnud väga informatiivne, ja nii me ei jätka ka; panin asemele 2 rida millega visuaalselt vaadata, kas eri arengustaadiumid E. affinisest on omavahel korreleerunud.

--- analysis_by_Riina.R
+++ analysis_by_Riina.R
@@ -5,27 +5,11 @@ if(user == "riina"){setwd("~/work/Heli/heli herring")}
 load("consolidated_herring.RData")
 
 #Kasutame alguses ainult neid ridu kus koik muutujad olemas
-d = herring[which(herring$complete == "yes"),]
-
+d = data[which(data$complete == "yes"),]
 
 plot(R~year, data = d)
 library(mgcv)
 
-full_m = gam(R ~ s(SSB1, k = 4)
-             + s(SSB0, k = 4)
-             + s(sun0, k = 4)
-             + s(E1_0, k = 4)
-             + s(E2_0, k = 4)
-             + s(E3_0, k = 4)
-             + s(L_0, k = 4)
-             + s(wa_prev_0, k = 4)
-             + s(wa_1, k = 4)
-             + s(may_0, k = 4)
-             + s(summer_0, k = 4),
-             data = d)
-
-summary(full_m)
-
-par(mfrow=c(3,4), mar = c(2.5,2.5,1,1), mgp = c(1.5,0.5,0))
-plot(full_m)
-
+#Kas toidu arengustaadiumid on omavahel seotud?
+pairs(d[,c(5:8)])
+pairs(d[,c(9:12)]) #Avaosas enam mitte nii väga
