de2e9275ff2d40491b3be10fe5f467c4bc4f69b1
klaispeets <69356602+klaispeets@users.noreply.github.com>
2020-08-28 14:43:26 +0300

    Update analysis_by_Riina.R

--- analysis_by_Riina.R
+++ analysis_by_Riina.R
@@ -5,12 +5,12 @@ if(user == "riina"){setwd("~/work/Heli/heli herring")}else{setwd("~/Desktop/Herr
 load("consolidated_herring.RData")
 
 #Kasutame alguses ainult neid ridu kus koik muutujad olemas
-d = data[which(data$complete == "yes"),]
+d = final[which(final$complete == "yes"),]
 d = d[which(d$year %in% c(1957:2015)),]
 
-d$N[which(d$year == 2002)] = mean(d$N[which(d$year %in% c(2001,2003))])
-d$E2[which(d$year == 1986)] = mean(d$E2[which(d$year %in% c(1985,1986,1987))])
-d$E1[which(d$year == 1986)] = mean(d$E1[which(d$year %in% c(1985,1986, 1987))])
+d$N[which(d$year == 2001)] = mean(d$N[which(d$year %in% c(2000,2002))])
+d$E2[which(d$year == 1987)] = mean(d$E2[which(d$year %in% c(1986,1987,1988))])
+d$E1[which(d$year == 1987)] = mean(d$E1[which(d$year %in% c(1986,1987,1988))])
 
 #Aegread
 
@@ -35,24 +35,24 @@ mtext("wa", side=3, adj=0, cex= 0.6)
 #Let's try the GAM models with interaction (tensor product)
 
 library(mgcv)
-summary(gam(R ~ te(N, SSB0, k = 4), data = d))#R2adj: 0.29
-summary(gam(R ~ te(E1, SSB0, k = 4), data = d))#0.245
-summary(gam(R ~ te(E2, SSB0, k = 4), data = d))#0.23
-summary(gam(R ~ te(E3, SSB0, k = 4), data = d))#0.23
-summary(gam(R ~ te(wa, SSB0, k = 4), data = d))#0.39# best one!
+summary(gam(R ~ te(N, SSB, k = 4), data = d))#R2adj: 0.29
+summary(gam(R ~ te(E1, SSB, k = 4), data = d))#0.245
+summary(gam(R ~ te(E2, SSB, k = 4), data = d))#0.23
+summary(gam(R ~ te(E3, SSB, k = 4), data = d))#0.23
+summary(gam(R ~ te(wa, SSB, k = 4), data = d))#0.39# best one!
 
 #Create 2D image of the model
 library(colorRamps)
 var = d$R
-SSB0 = d$SSB0
+SSB = d$SSB
 wa = d$wa
-m = gam(var ~ te(SSB0, wa))
-SSB0 = seq(min(d$SSB0), max(d$SSB0), length.out = 100)
+m = gam(var ~ te(SSB, wa))
+SSB = seq(min(d$SSB), max(d$SSB), length.out = 100)
 wa = seq(min(d$wa), max(d$wa), length.out = 100)
-predicted = matrix(ncol=length(SSB0),nrow = length(wa))
+predicted = matrix(ncol=length(SSB),nrow = length(wa))
 for(i in 1:length(wa)){ 
-  new.data = data.frame(SSB0,wa[i])
-  names(new.data)<-c("SSB0","wa")
+  new.data = data.frame(SSB,wa[i])
+  names(new.data)<-c("SSB","wa")
   pred = predict.gam(m, newdata = new.data)
   predicted[,i] = pred}
 
@@ -61,7 +61,7 @@ image(predicted,col=matlab.like(20),axes=F , xlab="Winter harshness", ylab = "SS
 contour(predicted,levels = c(1000,1500,2000,2500,3000,3500),add=T)
 
 labels_wa = as.numeric(c(round(quantile(wa, c(0.2,0.4,0.6,0.8)), digits = 0)))
-labels_SSB = as.numeric(c(round(quantile(SSB0, c(0.2,0.4,0.6,0.8)), digits = 0)))
+labels_SSB = as.numeric(c(round(quantile(SSB, c(0.2,0.4,0.6,0.8)), digits = 0)))
 
 axis(1, at = c(0.2,0.4,0.6,0.8), labels = labels_wa)
 axis(2, at = c(0.2,0.4,0.6,0.8), labels = labels_SSB)
@@ -70,34 +70,34 @@ axis(2, at = c(0.2,0.4,0.6,0.8), labels = labels_SSB)
 
 
 #Step2
-summary(gam(R ~ te(wa, SSB0, k = 4) + te(N, SSB0, k = 4), data = d))#0.42, marignally sign.
-summary(gam(R ~ te(wa, SSB0, k = 4) + te(E1, SSB0, k = 4), data = d))#NS
-summary(gam(R ~ te(wa, SSB0, k = 4) + te(E2, SSB0, k = 4), data = d))#NS
-summary(gam(R ~ te(wa, SSB0, k = 4) + te(E3, SSB0, k = 4), data = d))#NS
+summary(gam(R ~ te(wa, SSB, k = 4) + te(N, SSB, k = 4), data = d))#0.42, marignally sign.
+summary(gam(R ~ te(wa, SSB, k = 4) + te(E1, SSB, k = 4), data = d))#NS
+summary(gam(R ~ te(wa, SSB, k = 4) + te(E2, SSB, k = 4), data = d))#NS
+summary(gam(R ~ te(wa, SSB, k = 4) + te(E3, SSB, k = 4), data = d))#NS
 
 #Step3: food in interaction with wa
-summary(gam(R ~ te(wa, SSB0, k = 4) + te(N, wa, k = 4), data = d))#NS
-summary(gam(R ~ te(wa, SSB0, k = 4) + te(E1, wa, k = 4), data = d))#NS
-summary(gam(R ~ te(wa, SSB0, k = 4) + te(E2, wa, k = 4), data = d))#NS
-summary(gam(R ~ te(wa, SSB0, k = 4) + te(E3, wa, k = 4), data = d))#NS
+summary(gam(R ~ te(wa, SSB, k = 4) + te(N, wa, k = 4), data = d))#NS
+summary(gam(R ~ te(wa, SSB, k = 4) + te(E1, wa, k = 4), data = d))#NS
+summary(gam(R ~ te(wa, SSB, k = 4) + te(E2, wa, k = 4), data = d))#NS
+summary(gam(R ~ te(wa, SSB, k = 4) + te(E3, wa, k = 4), data = d))#NS
 
 #Step 4: interaction between N and E3
-summary(gam(R ~ te(wa, SSB0, k = 4) + te(N, E3, k = 4), data = d))#Rsq adj 0.49 (from 0.39), but te(E3,N) is only marginally significant (0.07)
+summary(gam(R ~ te(wa, SSB, k = 4) + te(N, E3, k = 4), data = d))#Rsq adj 0.49 (from 0.39), but te(E3,N) is only marginally significant (0.07)
 
 #Create 2D images of te(wa, SSB0) and te(N, E3)
 var = d$R
-SSB0 = d$SSB0
+SSB = d$SSB
 wa = d$wa
 N = d$N
 E3 = d$E3
-m = gam(var ~ te(SSB0, wa) + te(N, E3))
-SSB0 = seq(min(SSB0), max(SSB0), length.out = 100)
+m = gam(var ~ te(SSB, wa) + te(N, E3))
+SSB = seq(min(SSB), max(SSB), length.out = 100)
 wa = seq(min(wa), max(wa), length.out = 100)
-predicted = matrix(ncol=length(SSB0),nrow = length(wa))
+predicted = matrix(ncol=length(SSB),nrow = length(wa))
 
 for(i in 1:length(wa)){ 
-  new.data = data.frame(SSB0,wa[i], mean(N), mean(E3))
-  names(new.data)<-c("SSB0","wa", "N", "E3")
+  new.data = data.frame(SSB,wa[i], mean(N), mean(E3))
+  names(new.data)<-c("SSB","wa", "N", "E3")
   pred = predict.gam(m, newdata = new.data)
   predicted[,i] = pred}
 
@@ -105,23 +105,23 @@ par(mfrow = c(2,1))
 image(predicted,col=matlab.like(20),axes=F , xlab="Winter harshness", ylab = "SSB")
 contour(predicted,levels = c(1000,1500,2000,2500,3000,3500),add=T)
 labels_wa = as.numeric(c(round(quantile(wa, c(0.2,0.4,0.6,0.8)), digits = 0)))
-labels_SSB = as.numeric(c(round(quantile(SSB0, c(0.2,0.4,0.6,0.8)), digits = 0)))
+labels_SSB = as.numeric(c(round(quantile(SSB, c(0.2,0.4,0.6,0.8)), digits = 0)))
 axis(1, at = c(0.2,0.4,0.6,0.8), labels = labels_wa)
 axis(2, at = c(0.2,0.4,0.6,0.8), labels = labels_SSB)
 
 var = d$R
-SSB0 = d$SSB0
+SSB = d$SSB
 wa = d$wa
 N = d$N
 E3 = d$E3
-m = gam(var ~ te(SSB0, wa) + te(N, E3))
+m = gam(var ~ te(SSB, wa) + te(N, E3))
 N = seq(min(N), max(N), length.out = 100)
 E3 = seq(min(E3), max(E3), length.out = 100)
 predicted = matrix(ncol=length(N),nrow = length(E3))
 
 for(i in 1:length(E3)){ 
-  new.data = data.frame(N,E3[i], mean(SSB0), mean(wa))
-  names(new.data)<-c("N", "E3","SSB0","wa")
+  new.data = data.frame(N,E3[i], mean(SSB), mean(wa))
+  names(new.data)<-c("N", "E3","SSB","wa")
   pred = predict.gam(m, newdata = new.data)
   predicted[,i] = pred}
 
@@ -137,10 +137,10 @@ axis(2, at = c(0.2,0.4,0.6,0.8), labels = labels_N)
 #Out of sample prediction ability and influential years
 #Aim of next step is to identify which model is the best in out of sample prediction category, and whether there are some years that seem to affect the model most
 
-newdata = subset(d, select = c("year","R", "SSB0", "wa", "N","E1","E2","E3"))
+newdata = subset(d, select = c("year","R", "SSB", "wa", "N","E1","E2","E3"))
 #2 model candidates
-m1 = gam(R ~ te(SSB0, wa, k = 4), data = newdata)
-m2 = gam(R ~ te(SSB0, wa, k = 4) +  te(N, E3, k = 4) , data = newdata)
+m1 = gam(R ~ te(SSB, wa, k = 4), data = newdata)
+m2 = gam(R ~ te(SSB, wa, k = 4) +  te(N, E3, k = 4) , data = newdata)
 
 newdata$pred_m1 = predict(m1)
 newdata$pred_m2 = predict(m2)
@@ -149,10 +149,10 @@ newdata$oos_R2 = NA
 newdata$corr1 = NA
 newdata$corr2 = NA
 
-for(i in 1:55){
+for(i in 1:53){
   # train new models leaving out year i
-  m1 = gam(R ~ te(SSB0, wa, k = 4), data = newdata[-i,]) 
-  m2 = gam(R ~ te(SSB0, wa, k = 4) +  te(N, E3, k = 4) , data = newdata[-i,])
+  m1 = gam(R ~ te(SSB, wa, k = 4), data = newdata[-i,]) 
+  m2 = gam(R ~ te(SSB, wa, k = 4) +  te(N, E3, k = 4) , data = newdata[-i,])
   
   #Predict R for all years, incl the year that was left out ("out of sample prediction")
   pred1 = predict(m1, newdata = newdata)  
@@ -186,20 +186,20 @@ lines(oos_R1~year, data = newdata, col = 2)
 lines(oos_R2~year, data= newdata, col = 3)
 text(c(1960,1960,1960), c(7000,6300,5600), labels = c("Observed", "Model 1", "Model 2"), col = c(1,2,3), pos = 4)
 
-
-#Test whether the correlation between R and SSB0 depends on the mean level of SSB0
+if(TRUE){
+#Test whether the correlation between R and SSB depends on the mean level of SSB
 type = "chronological" # alternative: "chronological" #lisasin siia } juurde, muidu jookseb kinni (Heli)
-if(type=="SSB0"){newdata = newdata[order(newdata$SSB0),]}else{newdata = newdata[order(newdata$year),]}
-if(type=="SSB0"){xlabel = "Mean SSB"}else{xlabel = "Middle year"}
-meanSSB = rep(NA,41)
-slope = rep(NA, 41)
-p = rep(NA, 41)
-for(i in 1:41){
+if(type=="SSB"){newdata = newdata[order(newdata$SSB),]}else{newdata = newdata[order(newdata$year),]}
+if(type=="SSB"){xlabel = "Mean SSB"}else{xlabel = "Middle year"}
+meanSSB = rep(NA,39)
+slope = rep(NA, 39)
+p = rep(NA, 39)
+for(i in 1:39){
   idx = c(i: (i+14))
-  m = lm(R ~ SSB0, data = newdata[idx,])
+  m = lm(R ~ SSB, data = newdata[idx,])
   slope[i] = summary(m)$coefficients[2,1]
   p[i] = summary(m)$coefficients[2,4]
-  if(type=="SSB0"){meanSSB[i] = round(mean(newdata$SSB0[idx]), digits=0)}else{meanSSB[i] = round(mean(newdata$year[idx]), digits=0)}
+  if(type=="SSB"){meanSSB[i] = round(mean(newdata$SSB[idx]), digits=0)}else{meanSSB[i] = round(mean(newdata$year[idx]), digits=0)}
 }
 
 
@@ -210,14 +210,14 @@ points(slope[idx]~meanSSB[idx], pch = 16)
 abline(h = 0, lwd = 0.5)
 mtext(" a)", side = 3, adj = 0, line = -1.5, cex = 0.9)
 par(new=T)
-plot(newdata$SSB0 ~ newdata$year, type = "line", col = grey(0.5), axes = F, ylab = "", xlab = "")
+plot(newdata$SSB ~ newdata$year, type = "line", col = grey(0.5), axes = F, ylab = "", xlab = "")
 axis(4);mtext("Mean SSB", side = 4,  line = 1.5, cex = 0.8, col = grey(0.5))
 
 #WA
-slope = rep(NA, 41)
-p = rep(NA, 41)
-meanWA = rep(NA,41)
-for(i in 1:41){
+slope = rep(NA, 39)
+p = rep(NA, 39)
+meanWA = rep(NA,39)
+for(i in 1:39){
   idx = c(i: (i+14))
   m = lm(R ~ wa, data = newdata[idx,])
   slope[i] = summary(m)$coefficients[2,1]
@@ -235,10 +235,10 @@ axis(4);mtext("Mean wa", side = 4,  line = 1.5, cex = 0.8)
 
 
 #N
-slope = rep(NA, 41)
-p = rep(NA, 41)
-meanN = rep(NA,41)
-for(i in 1:41){
+slope = rep(NA, 39)
+p = rep(NA, 39)
+meanN = rep(NA,39)
+for(i in 1:39){
   idx = c(i: (i+14))
   m = lm(R ~ N, data = newdata[idx,])
   slope[i] = summary(m)$coefficients[2,1]
@@ -255,10 +255,10 @@ plot(meanN ~ meanSSB, type = "line", col = grey(0.5), axes = F, ylab = "", xlab 
 axis(4);mtext("Mean N", side = 4,  line = 1.5, cex = 0.8)
 
 #E1
-slope = rep(NA, 41)
-p = rep(NA, 41)
-meanE1 = rep(NA,41)
-for(i in 1:41){
+slope = rep(NA, 39)
+p = rep(NA, 39)
+meanE1 = rep(NA,39)
+for(i in 1:39){
   idx = c(i: (i+14))
   m = lm(R ~ E1, data = newdata[idx,])
   slope[i] = summary(m)$coefficients[2,1]
@@ -275,10 +275,10 @@ plot(meanN ~ meanSSB, type = "line", col = grey(0.5), axes = F, ylab = "", xlab 
 axis(4);mtext("Mean E1", side = 4,  line = 1.5, cex = 0.8)
 
 #E2
-slope = rep(NA, 41)
-p = rep(NA, 41)
-meanE2 = rep(NA,41)
-for(i in 1:41){
+slope = rep(NA, 39)
+p = rep(NA, 39)
+meanE2 = rep(NA,39)
+for(i in 1:39){
   idx = c(i: (i+14))
   m = lm(R ~ E2, data = newdata[idx,])
   slope[i] = summary(m)$coefficients[2,1]
@@ -296,10 +296,10 @@ axis(4);mtext("Mean E2", side = 4,  line = 1.5, cex = 0.8)
 
 
 #E3
-slope = rep(NA, 41)
-p = rep(NA, 41)
-meanE3 = rep(NA,41)
-for(i in 1:41){
+slope = rep(NA, 39)
+p = rep(NA, 39)
+meanE3 = rep(NA,39)
+for(i in 1:39){
   idx = c(i: (i+14))
   m = lm(R ~ E3, data = newdata[idx,])
   slope[i] = summary(m)$coefficients[2,1]
@@ -314,3 +314,4 @@ mtext(" f)", side = 3, adj = 0, line = -1.5, cex = 0.9)
 par(new=T)
 plot(meanE3 ~ meanSSB, type = "line", col = grey(0.5), axes = F, ylab = "", xlab = "")
 axis(4);mtext("Mean E3", side = 4,  line = 1.5, cex = 0.8)
+}
